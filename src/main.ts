385775fa2d5d28e6c359f58a59bfc5858635d7c7
Rega HR <rega.hruzty@gmail.com>
2024-06-29 01:01:16 +0700

    configure port and allow origin for FE_UEL

--- src/main.ts
+++ src/main.ts
@@ -3,19 +3,23 @@ import { createYoga } from "graphql-yoga";
 import { schema } from "./schema";
 import { createContext } from "./context";
 
+const port = process.env.PORT || 4000;
+const origin = [
+  "http://localhost:4000",
+  "http://localhost:3000",
+  ...(process.env.FE_URL ? process.env.FE_URL : []),
+];
 function main() {
   const yoga = createYoga({
     cors: {
-      origin: ["http://localhost:4000", "http://localhost:3000"],
-      allowedHeaders: ["X-Custom-Header", "Content-Type"],
-      methods: ["POST", "GET"],
+      origin: origin,
     },
     schema,
     context: createContext,
   });
   const server = createServer(yoga);
-  server.listen(4000, () => {
-    console.info("Server is running on http://localhost:4000/graphql");
+  server.listen(port, () => {
+    console.info(`Server is running on http://localhost:${port}/graphql`);
   });
 }
 
